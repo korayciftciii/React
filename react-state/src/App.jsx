56b0c16b8c9834511732bb2680023c8cc2ac3ea3
korayciftci <korayciftciii@users.noreply.github.com>
2025-04-09 18:16:37 +0300

    refactor: remove App.css and implement sculpture carousel in App component

--- react-state/src/App.jsx
+++ react-state/src/App.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import { useState } from 'react';
+import {sculptureList} from './data.js'
 function App() {
-  const [count, setCount] = useState(0)
-
+const[index,setIndex]= useState(0);
+const[showMore,setShowMore]= useState(false);
+  let sculpture = sculptureList[index];
+function handleNextClick() {
+  if(index<sculptureList.length-1){ 
+    setIndex(index +1);
+   }
+   else{
+    setIndex(0);
+   }
+  }
+function handlePreviousClick() {
+ if(index>0){
+    setIndex(index -1);
+  }
+  else{
+    setIndex(sculptureList.length-1);
+  }
+}
+function handleMoreClick() {
+  setShowMore(!showMore);
+}
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={handlePreviousClick}>Previous</button>
+      <button onClick={handleNextClick}>Next</button>
+      <h2>
+        {sculpture.name} by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img src={sculpture.url} alt={sculpture.alt} />
+      <br />
+      <button onClick={handleMoreClick}>{showMore ? "Hide" :"Show"} Details</button>
+     {showMore && <p>{sculpture.description}</p>} 
     </>
   )
 }
