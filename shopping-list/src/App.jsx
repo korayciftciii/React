757fdf39976fc282fd61f2ccd96a334cf244b823
korayciftci <korayciftciii@users.noreply.github.com>
2025-04-11 15:10:09 +0300

    refactor: remove unused App.css and restructure App component with shopping list functionality

--- shopping-list/src/App.jsx
+++ shopping-list/src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const items=[
+  {id:1, name:'item1', quantity:1, completed:false},
+  {id:2, name:'item2', quantity:2, completed:false},
+  {id:3, name:'item3', quantity:1, completed:false},
+  {id:4, name:'item4', quantity:4, completed:true},
+  {id:5, name:'item5', quantity:2, completed:true},
+];
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+ <div className='app'>
+     <Header/>
+    <Form/>
+    <List/>
+  </div>
+  );
 }
 
+function Header(){
+return(
+  <h1>🛒 Shopping List</h1>
+);
+}
+function Form(){
+return(
+  <form className='form'>
+    <input type="text" placeholder="Add item" />
+    <select >
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+    <button type="submit">Add</button>
+  </form>
+);
+}
+
+function List(){
+  return(
+    <div className='list'>
+    <ul>
+          {items.map((item,index) => (<Item item={item} key={index}/>))}
+    </ul>  
+    </div>
+  );
+}
+function Item({item}){
+  return(
+  <li>
+    <span style={item.completed ? {textDecoration:"line-through"} : {}}>{item.name}</span>
+    <button>X</button>
+  </li>
+  );
+}
+
+function Footer(){
+  return(
+    <footer>
+      <p>You have 10 products in your cart</p>
+    </footer>
+  );
+}
 export default App
