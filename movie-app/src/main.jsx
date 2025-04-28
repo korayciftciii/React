dec9d3104926373deb2e8473a63495da7e4d76c2
korayciftci <korayciftciii@users.noreply.github.com>
2025-04-28 09:07:55 +0300

    feat: add StartRating component for user rating functionality in the app

--- movie-app/src/main.jsx
+++ movie-app/src/main.jsx
@@ -4,9 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './index.css'
 import App from './App.jsx'
+import StartRating from './StartRating.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+    <StartRating maxRating={7} />
   </StrictMode>,
 )
