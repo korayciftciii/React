bf02808d018559d281b012162ee54c0b19d572c6
korayciftci <korayciftciii@users.noreply.github.com>
2025-05-11 15:31:31 +0300

    Initialize React MUI project with Vite, add basic components for Button, TextField, and Typography usage, and set up ESLint for code quality.

---
+++ react-mui/starter/react-mui/src/App.jsx
@@ -0,0 +1,16 @@
+import Button from './components/Button';
+import TypographyUsage from './components/TypographyUsage';
+import TextFieldUsage from './components/TextFieldUsage';
+function App() {
+  return (
+    <>
+      <TextFieldUsage>
+
+      </TextFieldUsage>
+      {/* <Button /> */}
+      {/* <TypographyUsage /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ react-mui/starter/react-mui/src/components/TypographyUsage.jsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material"
+
+export default function TypographyUsage() {
+    return (
+        <Box>
+            <Typography variant="h1">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, ut.
+            </Typography>
+        </Box>
+    )
+}
